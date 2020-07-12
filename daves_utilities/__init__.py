f6785562fcd940dbde0d0ed759104e7e0aa4b462
kuchedav <kuchelmeister.david@gmail.com>
2020-07-12 20:42:27 +0200

    add init configurations

--- daves_utilities/__init__.py
+++ daves_utilities/__init__.py
@@ -2,4 +2,4 @@
 Daves utilities is a collection of useful python functions which I would like to provide to anybody who is insterested in using them.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
