4ab09fc8a8763738785a829278fced97926bb6da
kuchedav <kuchelmeister.david@gmail.com>
2020-07-26 19:17:52 +0200

    fun_save, saves multiple runs now

--- daves_utilities/fun_save.py
+++ daves_utilities/fun_save.py
@@ -1,38 +1,60 @@
+# set working directory to correct path
 import os
+os.chdir("/Users/davidkuchelmeister/Documents/Projects_Coding/TMP_projects/daves_utilities")
+
 import pickle
-from .is_equal import is_equal
+from is_equal import is_equal
 
 def fun_save(fun_input, attr_input, path = "./", identifier : str = ""):
 
-    if identifier!="": identifier = "_" + identifier
-    save_path = f"{path}/{fun_input.__name__}{identifier}.pkl"
-    recalculate = True
+    # create hidden folder under path to save function outputs
+    fun_save_path = path + ".fun_save"
+    if not os.path.exists(fun_save_path):
+        os.makedirs(fun_save_path)
 
-    # check if output already exists
-    if os.path.isfile(save_path):
-        with open(save_path,"rb") as f:
+    # add underline to identifier if it exists
+    identifier = "_" + identifier if identifier else identifier
+
+    # structure to check pas calculations
+    save_structure = f"{fun_input.__name__}{identifier}"
+    files_found = [f"{fun_save_path}/{i}" for i in os.listdir(fun_save_path) if save_structure in i]
+
+    file_id = 0
+    # open all files and compare attributes, return if same input/output is found
+    for iter_path in files_found:
+        # read files
+        with open(iter_path,"rb") as f:
             output = pickle.load(f)
-        if is_equal.is_equal(output["attr_input"],attr_input):
-            print("Function file has been found and reused: " + str(fun_input.__name__) + identifier)
-            recalculate = False
+        
+        # check if input is the same
+        if is_equal(output["attr_input"],attr_input):
+            print("Function file has been found and reused: " + save_structure)
             return output["fun_output"]
-        else:
-            print("File exists but input attributes are different => recalculating function now: "  + str(fun_input.__name__) + identifier)
+        
+        # increament id if larger
+        file_id_iter = int(iter_path.split("_")[-1].replace(".pkl",""))
+        file_id = file_id_iter if file_id_iter > file_id else file_id
 
-    if recalculate:
-        # run long running function
-        fun_output = fun_input(**attr_input)
-        output = {"attr_input":attr_input,"fun_output":fun_output}
+    # increment file_id
+    file_id += 1
+    print("Input/Output combination not found => recalculating function now: "  + save_structure)
 
-        # save long running function
-        with open(save_path,"wb") as f:
-            pickle.dump(output,f,protocol=4)
+    # create full file name with identifier for multiple input variations
+    save_name = f"{fun_save_path}/{save_structure}_{file_id}.pkl"
 
-        return output["fun_output"]
+    # run function
+    fun_output = fun_input(**attr_input)
+    output = {"attr_input":attr_input,"fun_output":fun_output}
+
+    # save function output
+    with open(save_name,"wb") as f:
+        pickle.dump(output,f,protocol=4)
+
+    return output["fun_output"]
 
 if __name__=="__main__":
 
-    input_values = {"embeddings":["tf-idf","word2vec","doc2vec","bert"]}
+    input_values = {"embeddings":["tf-idf","word2vec","bert"]}
 
     # function to apply
     import time
@@ -40,4 +62,6 @@ if __name__=="__main__":
         time.sleep(1)
         return f"{embeddings}"
 
-    fun_save(fun_input = concatenate_input, attr_input = input_values)
+    output = fun_save(fun_input = concatenate_input, attr_input = input_values, path = "./daves_utilities/data/", identifier = "")
+
+    print(output)
